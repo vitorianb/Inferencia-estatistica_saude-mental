992f1d2a83431cafc41ea7a3551897faae3f2c6c
Vitoria Novello <vitorianbernardino@gmail.com>
2023-04-15 20:23:02 -0300

    Data

---
+++ structure/imports.py
@@ -0,0 +1,10 @@
+#Libs
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Data
+
+pag_hei = pd.read_csv("C:/Users/vitoria.novello_ifoo/Documents/tera/Inferencia-estatistica_saude-mental/data/PAG_HEI.csv")
+demo_php = pd.read_csv("C:/Users/vitoria.novello_ifoo/Documents/tera/Inferencia-estatistica_saude-mental/data/DEMO_PHQ.csv")
