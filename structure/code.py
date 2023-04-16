7598197e307623959d4250cff6b0c59d9d83fb64
Vitoria Novello <vitorianbernardino@gmail.com>
2023-04-16 09:24:57 -0300

    Ajustando estrutura

--- structure/code.py
+++ structure/code.py
@@ -1,10 +1,12 @@
-#Libs
+#imports
+
+##Libs
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-#Data
-
+##Data
 pag_hei = pd.read_csv("C:/Users/vitoria.novello_ifoo/Documents/tera/Inferencia-estatistica_saude-mental/data/PAG_HEI.csv")
 demo_php = pd.read_csv("C:/Users/vitoria.novello_ifoo/Documents/tera/Inferencia-estatistica_saude-mental/data/DEMO_PHQ.csv")
+
